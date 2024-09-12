bce120e4eb4ea4b08150e08be617feb9389dc1bf
Tetroit <shichinov.ivan@gmail.com>
2024-09-12 13:37:35 +0200

    DONT CREATE NEW BRANCHES FFS

--- ProjectCustomer/Assets/Scenes/Ivan/Scripts/GameBrain.cs
+++ ProjectCustomer/Assets/Scenes/Ivan/Scripts/GameBrain.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameState
+{
+    MENU,
+    SETTINGS,
+    INVENTORY,
+
+}
 public class GameBrain : MonoBehaviour
 {
     public static GameBrain main;
