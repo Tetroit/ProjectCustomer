5f97f9b047211212e511443d70b2740feed7380f
Aleksandar <549190@student.saxion.nl>
2024-09-12 13:06:36 +0200

    Took your controls and fixed camera-update/fix

--- ProjectCustomer/Assets/Scenes/Alex/Scripts/PlayerControls.cs
+++ ProjectCustomer/Assets/Scenes/Alex/Scripts/PlayerControls.cs
@@ -2,65 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum InputDevice
+{
+    Keyboard,
+    Joystick
+}
 public class PlayerControls : MonoBehaviour
 {
-    private float mouseSensitivity = 35f;
-    private float movementSpeed = 5f;
-    private float minAngle = 20f;
-    private float maxAngle = 35f;
+    public float mouseSensitivity = 10f;
+    public float movementSpeed = 5f;
+    public float minAngle = 20f;
+    public float maxAngle = 35f;
+    [Range(0f, 1f)]
+    public float cameraSmoothness = 0.5f;
+    public bool isMoving;
+
+    public InputDevice inputDevice;
 
     Vector2 turn;
+    Vector2 turnTarget;
 
     public Transform cameraTransform;
 
-    void Start()
+    private void Start()
     {
+        cameraTransform.rotation = transform.rotation;
 
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
-
     void Update()
     {
-        LookAround();
         Move();
+        LookAround();
+    }
+    private void FixedUpdate()
+    {
     }
 
     private void LookAround()
     {
-        turn.x += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        turn.y += Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        turnTarget.x += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        turnTarget.y += Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        turn.y = Mathf.Clamp(turn.y, -minAngle, maxAngle);
+        turnTarget.y = Mathf.Clamp(turnTarget.y, minAngle, maxAngle);
+        turn = Vector2.Lerp(turnTarget, turn, cameraSmoothness);
 
-        cameraTransform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0f);
-        transform.localRotation = Quaternion.Euler(0f, turn.x, 0f);
+        cameraTransform.rotation = Quaternion.Euler(-turn.y, turn.x, 0f);
+        transform.rotation = Quaternion.Euler(0f, turn.x, 0f);
     }
 
     private void Move()
     {
         Vector3 moveDirection = Vector3.zero;
 
-        if(Input.GetKey(KeyCode.W))
+        if (inputDevice == InputDevice.Joystick)
         {
-            moveDirection += transform.forward;
+            moveDirection += Input.GetAxisRaw("Vertical") * transform.forward;
+            moveDirection += Input.GetAxisRaw("Horizontal") * transform.right;
+        }
+        if (inputDevice == InputDevice.Keyboard)
+        {
+            if (Input.GetKey(KeyCode.W)) moveDirection += transform.forward;
+            if (Input.GetKey(KeyCode.S)) moveDirection -= transform.forward;
+            if (Input.GetKey(KeyCode.D)) moveDirection += transform.right;
+            if (Input.GetKey(KeyCode.A)) moveDirection -= transform.right;
         }
 
-        if(Input.GetKey(KeyCode.A))
-        {
-            moveDirection += -transform.right;
-        }
-
-        if(Input.GetKey(KeyCode.S))
-        {
-            moveDirection += -transform.forward;
-        }
-
-        if(Input.GetKey(KeyCode.D))
-        {
-            moveDirection += transform.right;
-        }
+        if (moveDirection.magnitude > 0.1f) isMoving = true;
+        else isMoving = false;
 
         moveDirection = moveDirection.normalized * movementSpeed * Time.deltaTime;
 
-        transform.Translate(moveDirection, Space.Self);
+        transform.position += moveDirection;
     }
 }
--- ProjectCustomer/Assets/Scenes/Ivan/Scripts/PlayerControlsIvan.cs
+++ ProjectCustomer/Assets/Scenes/Ivan/Scripts/PlayerControlsIvan.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum InputDevice
+public enum EInputDevice
 {
     Keyboard,
     Joystick
