7d05a2fdcbb93f28607959be20d16911893435cf
Tetroit <shichinov.ivan@gmail.com>
2024-09-16 20:56:32 +0200

    MORE DISTORTION

--- ProjectCustomer/Assets/Scenes/Ivan/Scripts/NoiseManager.cs
+++ ProjectCustomer/Assets/Scenes/Ivan/Scripts/NoiseManager.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+
 
 [ExecuteInEditMode]
 public class NoiseManager : MonoBehaviour
@@ -11,13 +11,70 @@ public class NoiseManager : MonoBehaviour
     public ScriptableRendererFeature _noise;
     public Material _noiseMaterial;
     public bool showInEditor = false;
-    void Start()
+
+    [Range(0f, 1f)]
+    public float fac = 0;
+    public float intensity
     {
+        get { return _noiseMaterial.GetFloat("_NoiseScale"); }
+        set { _noiseMaterial.SetFloat("_NoiseScale", value); }
+    }
+    public float distanceScale
+    {
+        get { return _noiseMaterial.GetFloat("_DistanceScale"); }
+        set { _noiseMaterial.SetFloat("_DistanceScale", value); }
+    }
+    public float distanceShift
+    {
+        get { return _noiseMaterial.GetFloat("_DistanceShift"); }
+        set { _noiseMaterial.SetFloat("_DistanceShift", value); }
+    }
+    public float scale
+    {
+        get { return _noiseMaterial.GetFloat("_NoiseResolution"); }
+        set { _noiseMaterial.SetFloat("_NoiseResolution", value); }
+    }
+    public float speed
+    {
+        get { return _noiseMaterial.GetFloat("_NoiseSpeed"); }
+        set { _noiseMaterial.SetFloat("_NoiseSpeed", value); }
+    }
+    public bool noisePreview
+    {
+        get { return _noiseMaterial.GetInt("_NoisePreview") == 1; }
+        set { _noiseMaterial.SetFloat("_NoisePreview", value ? 1 : 0); }
+    }
+    public bool distancePreview
+    {
+        get { return _noiseMaterial.GetInt("_DistancePreview") == 1; }
+        set { _noiseMaterial.SetFloat("_DistancePreview", value ? 1 : 0); }
+    }
+    public Color color
+    {
+        get { return _noiseMaterial.GetColor("_NoiseColor"); }
+        set { _noiseMaterial.SetColor("_NoiseColor", value); }
     }
 
+    void Start()
+    {
+        if (Application.isPlaying)
+        {
+            //option 1
+            distanceShift = -0.2f;
+            intensity = 0.1f;
+            scale = 1000f;
+        }
+    }
     void Update()
     {
+        if (Application.isPlaying)
+        {
+            intensity = fac * 0.1f;
+            //option 1
+            distanceScale = fac * (Mathf.Sin(Time.time)*0.1f + 0.11f);
+            color = Color.Lerp(Color.gray, Color.black, Mathf.Sin(Time.time));
 
+        }
     }
 
     private void Awake()
@@ -39,6 +96,7 @@ public class NoiseManager : MonoBehaviour
                     DisableNoise();
             }
         }
+
     }
 
     public void EnableNoise()
