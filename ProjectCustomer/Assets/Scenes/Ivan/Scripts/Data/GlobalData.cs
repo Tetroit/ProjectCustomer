1a1deba2524d573fc26b2407f2f1ba921677c911
Aleksandar <549190@student.saxion.nl>
2024-09-19 13:17:36 +0200

    Revert "Work on Demo"
    This reverts commit f687dff06f91f909ea8571159ccf00812f2f8464.

--- ProjectCustomer/Assets/Scenes/Ivan/Scripts/Data/GlobalData.cs
+++ ProjectCustomer/Assets/Scenes/Ivan/Scripts/Data/GlobalData.cs
@@ -29,7 +29,7 @@ public class GlobalData : MonoBehaviour
     }
     void Update()
     {
-        //Debug.Log(UIHintFlags);
+        Debug.Log(UIHintFlags);
     }
 
     public static void UnlockWallet()
