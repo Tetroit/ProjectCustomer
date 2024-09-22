280ea56549a6df8be638ce07e972f6879c7249c0
Aleksandar <549190@student.saxion.nl>
2024-09-22 21:44:33 +0200

    It broke again AAdwadawaefafwefsef

--- ProjectCustomer/Assets/Scenes/Alex/Scripts/Dialogue.cs
+++ ProjectCustomer/Assets/Scenes/Alex/Scripts/Dialogue.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 
 [System.Serializable]
-public class Dialogue
+public class DialogueLine
 {
     public string name;
 
     [TextArea(3, 10)]
-    public string[] firstDialogue;
+    public string sentence;
+}
 
-    [TextArea(3, 10)]
-    public string[] secondDialogue;
-
+[System.Serializable]
+public class Dialogue
+{
+    public DialogueLine[] firstDialogue;
+    public DialogueLine[] secondDialogue;
 }
