fbf202511efa0f76873cc4aae04f5df74be0d10a
Tetroit <shichinov.ivan@gmail.com>
2024-09-25 02:50:45 +0200

    SHADOWS AAAAAAAAA

--- ProjectCustomer/Assets/Scenes/Ivan/Shaders/Functions.hlsl
+++ ProjectCustomer/Assets/Scenes/Ivan/Shaders/Functions.hlsl
@@ -1,17 +1,30 @@
 #ifndef CUSTOM_LIGHTING_INCLUDED
 #define CUSTOM_LIGHTING_INCLUDED
 
-void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out float Attenuation)
+//#pragma multi_compile _ _MAIN_LIGHT_SHADOWS _MAIN_LIGHT_SHADOWS_CASCADE _MAIN_LIGHT_SHADOWS_SCREEN
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out float Attenuation, out float ShadowAttenuation)
 {
 #ifdef SHADERGRAPH_PREVIEW
      Direction = normalize(float3(1.0f, 1.0f, 0.0f));
      Color = 1.0f;
      Attenuation = 1.0f;
+     ShadowAttenuation = 1.0f;
+#else  
+#if SHADOWS_SCREEN
+    float4 shadowPos = ComputeScreenPos(TransformWorldToHClip(WorldPos));
 #else
-    Light mainLight = GetMainLight();
+    float4 shadowPos = TransformWorldToShadowCoord(WorldPos);
+#endif
+    Light mainLight = GetMainLight(shadowPos, WorldPos, 1);
     Direction = mainLight.direction;
     Color = mainLight.color;
     Attenuation = mainLight.distanceAttenuation;
+    ShadowSamplingData shadowSampler = GetMainLightShadowSamplingData();
+    float shadowStrength = GetMainLightShadowStrength();
+    ShadowAttenuation = shadowStrength;
+    ShadowAttenuation = MainLightRealtimeShadow(shadowPos);
 #endif
 }
 
